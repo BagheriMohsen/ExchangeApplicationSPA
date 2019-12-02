46ef8dc78ab4f3f57a1786e2442e7efe7f010e6a
Mohsen Bagheri <bagheriimohsen@gmail.com>
2019-12-02 18:55:31 +0330

    bug Fix

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,11 @@
 | Auth Routes
 |--------------------------------------------------------------------------
 */
-<<<<<<< HEAD
 $router->group(['as'=>'auth.'], function () use ($router) {
     $router->get('/register', 'AuthController@register');
     $router->get('/login', 'AuthController@login');
 });
-/*
-|--------------------------------------------------------------------------
-| Home Routes
-|--------------------------------------------------------------------------
-*/
+
 $router->group(['as'=>'home.'], function () use ($router) {
     $router->get('/', 'HomeController@index');
 });
@@ -25,10 +20,14 @@ $router->group(['as'=>'home.'], function () use ($router) {
 */
 $router->group(['prefix' => '/admin/','middleware'=>'auth','as'=>'admin.'], function () use ($router) {
     $router->get('/', 'HomeController@index');
-=======
 
+});
+/*
+|--------------------------------------------------------------------------
+| User Panel Routes
+|--------------------------------------------------------------------------
+*/
 $router->get('/', function () use ($router) {
     return view('index');
->>>>>>> 89769b095d0b37866a06a83ec8dbad3abeb80216
 });
-// $router->get('/home', 'SpaController@index');
+
